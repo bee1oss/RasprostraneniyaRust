b9bc8df6362756841bf60d22028525cb12a6e9bd
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-17 14:59:14 +0300

    a

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::fs::File;
-use std::io::{Error, Read};
+use std::io::{Error, ErrorKind, Read};
 
 fn main() {
     let path = "file.txt";
     match read_file_date(path) {
-        Ok(data) =>println!("File data:{}",data),
-        Err(e) => panic!("Error occured:{e}")
+        Ok(data) => println!("File data:{}", data),
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(_) => println!("File create"),
+                Err(e) => panic!("Error creating file:{e}")
+            },
+            other=>panic!("Error occured:{:?}",other),
+        },
     }
 }
 
